76ef05c34ce71c266f1086bf795e2a86eea55750
devxap <devavinash2023@gmail.com>
2024-06-05 13:46:46 +0530

    Updated JWTTOKEN.JS

--- controllers/jwtToken.js
+++ controllers/jwtToken.js
@@ -4,6 +4,8 @@ const sendToken = (user, statusCode, res) =>{
     const options={
         expires:new Date(Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000),
         httpOnly:true,
+        secure:true,
+        sameSite:'Strict'
     };
 
     res.status(statusCode).cookie("token",token, options).json({
